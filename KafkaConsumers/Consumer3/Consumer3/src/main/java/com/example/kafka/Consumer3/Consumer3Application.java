37d7c16f5591db1cc91a0e2e880e8cd4b2243fdd
Roshan-kumar-040193 <Roshan.kumar24x7@gmail.com>
2024-10-07 23:55:57 +0530

    Kafka Consumers

---
+++ KafkaConsumers/Consumer3/Consumer3/src/main/java/com/example/kafka/Consumer3/Consumer3Application.java
@@ -0,0 +1,13 @@
+package com.example.kafka.Consumer3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Consumer3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Consumer3Application.class, args);
+	}
+
+}
