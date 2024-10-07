29bf3e85dcc94ea3b0d4ae1356fa31d6d1580134
Roshan-kumar-040193 <Roshan.kumar24x7@gmail.com>
2024-10-08 02:13:27 +0530

    Kafka consumer changes

---
+++ KafkaConsumers/Consumer1/Consumer1/src/main/java/com/example/kafka/Consumer1/ConsumerService/KafkaConsumerService.java
@@ -0,0 +1,13 @@
+package com.example.kafka.Consumer1.ConsumerService;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumerService {
+    @KafkaListener(topics="Spring-Topic-Auto")
+    public void ConsumeMessage(String message){
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println(message);
+    }
+}
---
+++ KafkaConsumers/Consumer2/Consumer2/src/main/java/com/example/kafka/Consumer2/ConsumerService/KafkaConsumerService.java
@@ -0,0 +1,13 @@
+package com.example.kafka.Consumer2.ConsumerService;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumerService {
+    @KafkaListener(topics="Spring-Topic-Auto")
+    public void ConsumeMessage(String message){
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println(message);
+    }
+}
